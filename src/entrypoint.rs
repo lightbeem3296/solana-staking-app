1cf647cb688916f91348a30832ba1f29739be94a
Billy Lights <alpha5611331@gmail.com>
2024-05-29 05:46:02 -0400

    fixed errors

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -1,7 +1,7 @@
+use crate::processor::process;
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
 };
-use crate::processor::process;
 
 entrypoint!(process_instruction);
 fn process_instruction(
--- src/state.rs
+++ src/state.rs
@@ -1,17 +1,20 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::pubkey::Pubkey;
+use solana_program::{program_pack::IsInitialized, pubkey::Pubkey};
 
 /// Define the type of state stored in
 #[derive(BorshDeserialize, BorshSerialize, Debug)]
 pub struct PoolStorageAccount {
     pub pool_authority: Pubkey,
+
     pub total_staked: u64,
     pub user_count: u64,
     pub rewards_per_token: u64,
+
+    pub is_initialized: bool,
 }
 
-impl IsInitialized<PoolStorageAccount> for PoolStorageAccount {
+impl IsInitialized for PoolStorageAccount {
     fn is_initialized(&self) -> bool {
-        return false;
+        self.is_initialized
     }
 }
